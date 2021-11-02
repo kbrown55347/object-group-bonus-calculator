b825f05d47d43ac676f8e38918724a235797d752
Kelsey Brown <kbrown55347@gmail.com>
2021-11-02 16:33:15 -0500

    Completed base goals of bonus calculator group project

--- client.js
+++ client.js
@@ -1,3 +1,10 @@
+// attempted stretch goal
+// $(document).ready(readyNow);
+
+// function readyNow(){
+
+// } // end readyNow
+
 const employees = [
   {
     name: 'Atticus',
@@ -39,4 +46,62 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+// for of loop 
+for (let employee of employees) {
+  console.log(getEmployeeInfo(employee));
+}
+
+
+function getEmployeeInfo(employee) {
+  let bonus = employee.annualSalary * (bonusPercentage(employee)/100);
+  let employeeInformation = {
+    name: employee.name, 
+    bonusPercentage: bonusPercentage(employee) + '%',
+    totalCompensation: Number(employee.annualSalary) + bonus,
+    totalBonus: bonus,
+  }
+  
+  return employeeInformation;
+} // end getEmployeeInfo
+
+function bonusPercentage(employee) {
+  let percentage = 0;
+  if (employee.reviewRating === 3) {
+    percentage += 4;
+  } else if (employee.reviewRating === 4) {
+    percentage += 6;
+  } else if (employee.reviewRating === 5) {
+    percentage += 10;
+  }
+  if (employee.employeeNumber.length === 4){
+    percentage += 5;
+  }
+  if (employee.annualSalary > 65000){
+    percentage -= 1;
+  }
+  if (percentage >13){
+    percentage = 13;
+  }
+  if (percentage < 0 ){
+    percentage = 0;
+  }
+  return percentage;
+} // end bonusPercentage
+
+
+// attempted stretch goal
+// function displayEmployeeInfo(employees) {
+//   for (let employee of employees) {
+//     $('#annualEpr').append(`<li>${getEmployeeInfo(employee)}</li>`);
+//   }
+// }
+
+// console.log(bonusPercentage(employees[0]));
+// console.log(bonusPercentage(employees[1]));
+// console.log(bonusPercentage(employees[2]));
+// console.log(bonusPercentage(employees[3]));
+// console.log(bonusPercentage(employees[4]));
+
 console.log( employees );
+
+
